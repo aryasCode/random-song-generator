6971a19c4451880780c09c4a4e983b2bca0fae2d
Caleah <caleah77@gmail.com>
2019-07-12 16:15:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// array songs = []
+
+var songlist = [
+    "Billie Eilish: Ocean Eyes"
+    "Alesso: Remedy"
+    "James Bay: Let It Go"
+    "Ed Sheeran and Chance the Rapper: Cross Me"
+    "Mark Ronson and Miley Cyrus: Nothing Breaks Like A Heart"
+    "Zayn and Sia: Dusk Till Dawn"
+    "Zayn and Nicki Minaj: No Candle No Light"
+    "Selena Gomez: Killem With Kindness"
+    "Selena Gomez: Slow Down"
+    "One Republic: Rescue Me"
+    "Katy Perry: Never Really Over"
+    "Taylor Swift: Wildest Dreams"
+    "Taylor Swift: Blank Space"
+    "Ozuna: Baila Baila Baila"
+    "Camilla Cabello: Crying in the Club"
+    "Taylor Swift and Brendon Urie: ME!"
+    "Lauv and Troye Sivan: Im so Tired"
+    "BTS: Fake Love"
+    "Justin Bieber: Friends"
+    "Justin Bieber: Life is Worth Living"
+    "Dean Lewis: Waves"
+    "Rihanna: Diamonds"
+    "NOTD and Bea Miller: I Wanna Know"
+    "Major Lazor & DJ Snake: Lean On"
+    "The Weeknd and Daft Punk: Staryboy"
+    "Drake: Going Bad"
+    "Drake: Yes Indeed"
+    "Madison Beer: Home With You"
+    "Jennifer Lopez: Aint Your Mama"
+    "Taylor Swift: Style"
+    "Kendrick Lamar: HUMBLE"
+    "Alan Walker: Faded"
+    "Khalid: Young, Dumb, and Broke"
+    "Mike Posner: I Took a Pill in Ibiza"
+    "Jess Glynne: I'll Be There"
+
+
+
+
+
+
+
+
+
+
+
+]
